0920af0723e412f78c43da2781116a2c94194438
carlosm006gc <104165201+carlosm006gc@users.noreply.github.com>
2022-10-13 10:47:45 -0300

    classes user e usercontroler criados

---
+++ src/main/java/com/dev/curso/controllers/UserController.java
@@ -0,0 +1,20 @@
+package com.dev.curso.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1l, "Carlos", "carlos@gmail.com", "61999656158", "carlos8245");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
