ac29056179bd4569c2acf368ac497cc1cf8dbf7f
carlosm006gc <104165201+carlosm006gc@users.noreply.github.com>
2022-10-17 19:05:16 -0300

    method update, delete e post implementados

--- src/main/java/com/dev/curso/services/UserService.java
+++ src/main/java/com/dev/curso/services/UserService.java
@@ -14,15 +14,33 @@ public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	public List<User> findAll() {
 		return userRepository.findAll();
 	}
+
 	public User findById(Long id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.get();
 	}
+
 	public User insert(User user) {
 		return userRepository.save(user);
+	}
+
+	public void delete(Long id) {
+		userRepository.deleteById(id);
+	}
+
+	public User update(Long id, User user) {
+		User entity = userRepository.getReferenceById(id);
+		updateData(entity, user);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User user) {
+		entity.setName(user.getName());
+		entity.setEmail(user.getEmail());
+		entity.setPhone(user.getPhone());
 	}
 }
